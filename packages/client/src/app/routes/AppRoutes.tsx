91745dc2174b7c70c9232f976948d6ba2d1546c9
Helena Suslenkova <helena.suslenkova@gmail.com>
2023-06-23 21:27:49 +0300

    chore: add router

--- packages/client/src/app/routes/AppRoutes.tsx
+++ packages/client/src/app/routes/AppRoutes.tsx
@@ -1,1 +1,43 @@
-export const AppRoutes = (): JSX.Element => <div>App Routes</div>;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  AppLayout,
+  AuthenticatedLayout,
+  NotAuthenticatedLayout,
+} from '@/layout';
+import { RouteConfig } from './types';
+import {
+  authenticatedRoutes,
+  commonRoutes,
+  notAuthenticatedRoutes,
+} from './helper';
+import { routes } from './routes';
+
+export const AppRoutes = (): JSX.Element => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route element={<AuthenticatedLayout />}>
+            {authenticatedRoutes(routes).map(
+              ({ title, component: Element, path }: RouteConfig) => (
+                <Route key={title} element={<Element />} path={path} />
+              )
+            )}
+          </Route>
+          <Route element={<NotAuthenticatedLayout />}>
+            {notAuthenticatedRoutes(routes).map(
+              ({ title, component: Element, path }: RouteConfig) => {
+                return <Route key={title} element={<Element />} path={path} />;
+              }
+            )}
+          </Route>
+          {commonRoutes(routes).map(
+            ({ title, component: Element, path }: RouteConfig) => (
+              <Route key={title} element={<Element />} path={path} />
+            )
+          )}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
