58e89cdd0698fc66bbc67342042b482dd5fc510c
Helena Suslenkova <helena.suslenkova@gmail.com>
2023-06-27 19:38:51 +0300

    feat: add theme object

--- packages/client/src/components/pageWrapper/PageWrapper.tsx
+++ packages/client/src/components/pageWrapper/PageWrapper.tsx
@@ -6,7 +6,15 @@ export const PageWrapper: FC<{
   children: ReactNode
   columns: number
 }> = ({ children, columns }) => (
-  <Container fluid p={0} m={0}>
+  <Container
+    fluid
+    py={16}
+    sx={theme => ({
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[8]
+          : theme.colors.gray[0],
+    })}>
     <SimpleGrid cols={columns} sx={{ gap: 'initial' }}>
       {children}
     </SimpleGrid>
--- packages/client/src/layout/authenticated-layout/AuthenticatedLayout.tsx
+++ packages/client/src/layout/authenticated-layout/AuthenticatedLayout.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react'
 import { Outlet } from 'react-router-dom'
 
+import { Paper } from '@mantine/core'
+
 export const AuthenticatedLayout: FC = (): JSX.Element => (
-  <>
+  <Paper shadow="xs" p="md">
     <Outlet />
-  </>
+  </Paper>
 )
