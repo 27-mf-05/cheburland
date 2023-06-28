32964468d4d9688d2b6e7bc26525f93a875dac95
aaa15test <aaa15test15aaa@mail.ru>
2023-06-28 16:49:02 +0300

    feat: add main page

--- packages/client/src/pages/main/Main.tsx
+++ packages/client/src/pages/main/Main.tsx
@@ -1,3 +1,84 @@
-import { Text } from '@mantine/core'
+import {
+  BackgroundImage,
+  Box,
+  Button,
+  Center,
+  Stack,
+  Tooltip,
+} from '@mantine/core'
 
-export const Main = (): JSX.Element => <Text>Main Page</Text>
+const gameDescription =
+  'Все мы прекрасно знаем, что лучшее лакомство для Чебурашки - это апельсин 🍊. Ваша цель - собрать как можно больше апельсинов, двигаясь по лабиринту. Апельсин может появляться в рандомном месте.'
+
+export const Main = (): JSX.Element => {
+  return (
+    <Box
+      sx={() => ({
+        margin: '-16px',
+      })}>
+      <BackgroundImage src="/cheburashka_background.jpg" h="100vh">
+        <Center
+          p="md"
+          h="100%"
+          sx={() => ({
+            background: 'rgba(0,0,0,0.4)',
+          })}>
+          <Stack align="flex-start" justify="flex-start" ml="-550px">
+            <Button size="lg" bg="brand.2" w={230} component="a" href="/game">
+              Начать игру
+            </Button>
+            <Tooltip
+              label={gameDescription}
+              bg="brand.0"
+              withArrow
+              multiline
+              width={350}>
+              <Button
+                size="lg"
+                mt="md"
+                variant="light"
+                bg="brand.3"
+                c="brand.2"
+                w={230}>
+                Описание игры
+              </Button>
+            </Tooltip>
+            <Button
+              size="lg"
+              mt="md"
+              variant="light"
+              bg="brand.3"
+              c="brand.2"
+              w={230}
+              component="a"
+              href="/profile">
+              Профиль
+            </Button>
+            <Button
+              size="lg"
+              mt="md"
+              variant="light"
+              bg="brand.3"
+              c="brand.2"
+              w={230}
+              component="a"
+              href="/leader-board">
+              Лидерборд
+            </Button>
+            <Button
+              size="lg"
+              mt="md"
+              variant="light"
+              bg="brand.3"
+              c="brand.2"
+              w={230}
+              component="a"
+              href="/forum">
+              Форум
+            </Button>
+          </Stack>
+        </Center>
+      </BackgroundImage>
+    </Box>
+  )
+}
