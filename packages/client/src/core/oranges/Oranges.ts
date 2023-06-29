57ee80ac1be1d46f0a9a220cae8adda627491eba
Georgy Pasyuk <pasyukgeorgy@gmail.com>
2023-06-29 23:25:02 +0300

    added core mechanics

---
+++ packages/client/src/core/oranges/Oranges.ts
@@ -0,0 +1,38 @@
+import Maze from "../maze/Maze";
+
+export default class Oranges {
+  private canvas: HTMLCanvasElement;
+  private context: CanvasRenderingContext2D;
+  private _cells: any[];
+  private _randomCell: Record<string, any>;
+  private maze;
+
+  constructor(maze: Maze) {
+    this.canvas = maze._canvas;
+    this.context = this.canvas.getContext("2d");
+    this._cells = maze.getTrueCells();
+    this.maze = maze;
+  }
+
+  getRandomCell(array) {
+    const index = Math.floor(Math.random() * array.length);
+    return array[index];
+  }
+
+  draw(state: boolean) {
+    if (state) {
+      this._randomCell = this.getRandomCell(this._cells);
+    }
+    this.context.beginPath();
+    this.context.rect(
+      this._randomCell.x * this.maze.CELL_SIZE + this.maze.PADDING + 20,
+      this._randomCell.y * this.maze.CELL_SIZE + this.maze.PADDING + 20,
+      20,
+      20
+    );
+
+    this.context.fillStyle = "orange";
+    this.context.fill();
+    this.context.closePath();
+  }
+}
--- packages/client/src/pages/registration/Registration.tsx
+++ packages/client/src/pages/registration/Registration.tsx
@@ -1,3 +1,7 @@
 import { Text } from '@mantine/core'
 
-export const Registration = (): JSX.Element => <Text>Registration Page</Text>
+export const Registration = (): JSX.Element => {
+  return (
+    <div></div>
+  )
+}
