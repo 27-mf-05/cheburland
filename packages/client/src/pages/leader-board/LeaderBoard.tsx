cf0ab3d5919af8163e63aefaff188931dd5ecaf3
Sergey Mironov <sergeymironov@protonmail.com>
2023-07-01 16:18:31 +0300

    chore: refactored LeaderBoard

--- packages/client/src/pages/leader-board/LeaderBoard.tsx
+++ packages/client/src/pages/leader-board/LeaderBoard.tsx
@@ -1,68 +1,20 @@
-import { useCallback, useState } from 'react'
-
 import { Container, Title } from '@mantine/core'
 
 import { LeaderBoardTable } from './LeaderBoardTable'
-import type { Leader, SortConfig } from './types'
+import type { Leader } from './types'
+
+const leaders: Leader[] = [
+  { id: '1', rank: 1, avatar: null, name: 'Player 1', score: 1000 },
+  { id: '2', rank: 2, avatar: null, name: 'Player 2', score: 800 },
+  { id: '3', rank: 3, avatar: null, name: 'Player 3', score: 500 },
+  { id: '4', rank: 4, avatar: null, name: 'Player 4', score: 300 },
+]
 
 export const LeaderBoard = (): JSX.Element => {
-  const [leaders, setLeaders] = useState<Leader[]>([
-    { rank: 1, avatar: null, name: 'Player 1', score: 1000 },
-    { rank: 2, avatar: null, name: 'Player 2', score: 800 },
-    { rank: 3, avatar: null, name: 'Player 3', score: 500 },
-    { rank: 4, avatar: null, name: 'Player 4', score: 300 },
-  ])
-
-  const [sortConfig, setSortConfig] = useState<SortConfig>({
-    column: null,
-    direction: 'asc',
-  })
-
-  const handleSort = useCallback(
-    (column: string) => {
-      let direction: 'asc' | 'desc' = 'asc'
-
-      if (sortConfig.column === column && sortConfig.direction === 'asc') {
-        direction = 'desc'
-      }
-
-      const sortedLeaders = [...leaders].sort((a, b) => {
-        const valueA = a[column as keyof Leader]
-        const valueB = b[column as keyof Leader]
-
-        if (valueA === null) {
-          return direction === 'asc' ? 1 : -1
-        }
-
-        if (valueB === null) {
-          return direction === 'asc' ? -1 : 1
-        }
-
-        if (valueA < valueB) {
-          return direction === 'asc' ? -1 : 1
-        }
-
-        if (valueA > valueB) {
-          return direction === 'asc' ? 1 : -1
-        }
-
-        return 0
-      })
-
-      setLeaders(sortedLeaders)
-      setSortConfig({ column, direction })
-    },
-    [leaders, sortConfig]
-  )
-
   return (
     <Container size="xl">
       <Title mb="xl">Лидерборд</Title>
-      <LeaderBoardTable
-        data={leaders}
-        sortConfig={sortConfig}
-        handleSort={handleSort}
-      />
+      <LeaderBoardTable data={leaders} />
     </Container>
   )
 }
