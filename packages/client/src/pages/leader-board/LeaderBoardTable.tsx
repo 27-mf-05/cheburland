8fa2a30eaaed0c44c8b2296fb3a254b8fdaa72c9
Sergey Mironov <sergeymironov@protonmail.com>
2023-07-01 16:18:31 +0300

    chore: refactored LeaderBoard

--- packages/client/src/pages/leader-board/LeaderBoardTable.tsx
+++ packages/client/src/pages/leader-board/LeaderBoardTable.tsx
@@ -1,81 +1,34 @@
 import type { FC } from 'react'
 
-import { Avatar, Button, Table } from '@mantine/core'
+import { Avatar } from '@mantine/core'
+import { DataTable } from 'mantine-datatable'
 
-import type { Leader, SortConfig } from './types'
+import type { Leader } from './types'
 
 type LeaderBoardTableProps = {
   data: Leader[]
-  sortConfig: SortConfig
-  handleSort: (column: string) => void
-}
-
-// Функция для определения цвета кнопки
-const getButtonColor = (column: string, sortConfig: SortConfig) => {
-  return sortConfig.column === column ? 'brand.2' : 'brand.1'
-}
-
-// Функция для определения символа сортировки
-const getSortSymbol = (column: string, sortConfig: SortConfig) => {
-  if (sortConfig.column === column) {
-    return sortConfig.direction === 'asc' ? '▲' : '▼'
-  }
-
-  return ''
 }
 
 export const LeaderBoardTable: FC<LeaderBoardTableProps> = ({
   data,
-  sortConfig,
-  handleSort,
 }): JSX.Element => {
   return (
-    <Table striped>
-      <thead>
-        <tr>
-          <th style={{ width: '1%' }}>
-            <Button
-              variant="subtle"
-              color={getButtonColor('rank', sortConfig)}
-              onClick={() => handleSort('rank')}
-              compact
-              uppercase>
-              № {getSortSymbol('rank', sortConfig)}
-            </Button>
-          </th>
-          <th style={{ width: '1%' }}></th>
-          <th>
-            <Button
-              variant="subtle"
-              color={getButtonColor('name', sortConfig)}
-              onClick={() => handleSort('name')}
-              compact
-              uppercase>
-              Имя {getSortSymbol('name', sortConfig)}
-            </Button>
-          </th>
-          <th>
-            <Button
-              variant="subtle"
-              color={getButtonColor('score', sortConfig)}
-              onClick={() => handleSort('score')}
-              compact
-              uppercase>
-              Счет {getSortSymbol('score', sortConfig)}
-            </Button>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(({ rank, avatar, name, score }) => (
-          <tr key={rank}>
-            <td>{rank}</td>
-            <td>{<Avatar src={avatar} alt={name} radius="xl" size="lg" />}</td>
-            <td>{name}</td>
-            <td>{score}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <DataTable
+      striped
+      records={data}
+      columns={[
+        { accessor: 'rank', title: '№', width: '1%' },
+        {
+          accessor: 'avatar',
+          title: '',
+          width: '1%',
+          render: ({ avatar, name }) => (
+            <Avatar src={avatar} alt={name} radius="xl" size="lg" />
+          ),
+        },
+        { accessor: 'name', title: 'Имя' },
+        { accessor: 'score', title: 'Счет' },
+      ]}
+    />
   )
 }
