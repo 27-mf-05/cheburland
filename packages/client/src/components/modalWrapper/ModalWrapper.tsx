a40eda27e551a808ffc2df60c50493be0a240c9a
aaa15test <aaa15test15aaa@mail.ru>
2023-07-01 16:56:43 +0300

    chore: remove comments

--- packages/client/src/components/modalWrapper/ModalWrapper.tsx
+++ packages/client/src/components/modalWrapper/ModalWrapper.tsx
@@ -1,7 +1,6 @@
 import { FC, ReactNode } from 'react'
 
 import { Modal } from '@mantine/core'
-// import { useDisclosure } from '@mantine/hooks'
 
 export const ModalWrapper: FC<{
   size: string
@@ -10,8 +9,6 @@ export const ModalWrapper: FC<{
   close: () => void
   children: ReactNode
 }> = ({ title = '', size = 'md', openned = false, close, children }) => {
-  // const [openned] = useDisclosure(isOpenned)
-
   return (
     <Modal size={size} opened={openned} onClose={close} title={title} centered>
       {children}
