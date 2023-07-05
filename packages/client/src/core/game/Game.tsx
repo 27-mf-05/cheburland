2e85e6c38d17535e35f76dea6503fb24e421317a
Georgy Pasyuk <pasyukgeorgy@gmail.com>
2023-07-05 22:27:07 +0300

    feat: Add game engine logic

---
+++ packages/client/src/core/game/Game.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useRef } from 'react'
+
+import { Hero, Maze, Oranges } from '@/core'
+
+export const Game = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const mazeRef = useRef<Maze | null>(null)
+  const heroRef = useRef<Hero | null>(null)
+  const orangesRef = useRef<Oranges | null>(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas?.getContext('2d')
+
+    if (!canvas || !context) {
+      return
+    }
+
+    const maze = new Maze(canvas, 13, 65, 10, 10)
+    mazeRef.current = maze
+
+    maze.generate().then(() => {
+      const cellSize = mazeRef.current!.cellSize
+      const hero = new Hero(
+        canvas,
+        mazeRef.current!.getFalseCells(),
+        cellSize,
+        mazeRef.current!.rows,
+        mazeRef.current!.columns
+      )
+      heroRef.current = hero
+
+      const oranges = new Oranges(
+        canvas,
+        mazeRef.current!.getTrueCells(),
+        cellSize
+      )
+      orangesRef.current = oranges
+
+      orangesRef.current?.draw(true)
+      animate()
+    })
+    return () => {
+      cancelAnimationFrame(animationFrame)
+    }
+  }, [])
+
+  let animationFrame: number
+
+  const animate = () => {
+    animationFrame = requestAnimationFrame(animate)
+    mazeRef.current?.drawMaze()
+    heroRef.current?.update()
+    if (
+      orangesRef.current?.collapseWithFruit(
+        heroRef.current!.position.x,
+        heroRef.current!.position.y
+      )
+    ) {
+      orangesRef.current!.draw(true)
+    }
+
+    orangesRef.current?.draw(false)
+  }
+
+  return <canvas ref={canvasRef} width={300} height={300} />
+}
---
+++ packages/client/src/core/index.ts
@@ -0,0 +1,4 @@
+export * from './game'
+export * from './hero'
+export * from './maze'
+export * from './oranges'
