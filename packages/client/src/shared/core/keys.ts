7fb9ac22d68d13906d31ae1a426c2fc2fae8871b
Georgy Pasyuk <pasyukgeorgy@gmail.com>
2023-07-06 00:23:44 +0300

    chore: refactoring

---
+++ packages/client/src/shared/core/keys.ts
@@ -0,0 +1,29 @@
+export const keys: { [key: number]: { pressed: boolean } } = {
+  87: {
+    pressed: false,
+  },
+  38: {
+    pressed: false,
+  },
+
+  65: {
+    pressed: false,
+  },
+  37: {
+    pressed: false,
+  },
+
+  83: {
+    pressed: false,
+  },
+  40: {
+    pressed: false,
+  },
+
+  68: {
+    pressed: false,
+  },
+  39: {
+    pressed: false,
+  },
+}
