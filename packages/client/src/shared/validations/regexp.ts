b3d2d818045fb63eaf1ebb18f26d9f1a49aeb09c
Helena Suslenkova <helena.suslenkova@gmail.com>
2023-07-05 15:08:30 +0300

    feat: add change password functionality

--- packages/client/src/shared/validations/regexp.ts
+++ packages/client/src/shared/validations/regexp.ts
@@ -3,5 +3,6 @@ export const secondNameRegexp = /^([A-Za-zА-Яа-яЁё][A-Za-zА-Яа-яЁё-]
 export const loginRegexp = /^(?!.*[-_]{2})[a-zA-Z0-9_-]{3,20}$/
 export const emailRegexp =
   /^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\\.[a-zA-Z]{2,}$/
-export const passwordRegexp = /^(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,40}$/
+
+export const passwordRegexp = /^(?=.*[A-Z])(?=.*\d).{8,40}$/
 export const phoneRegexp = /^\\+?\\d{10,15}$/
