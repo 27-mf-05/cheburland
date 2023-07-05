dd110e7761e04cfbc13082d8982d46e8cd773a38
aaa15test <aaa15test15aaa@mail.ru>
2023-07-05 23:45:23 +0300

    chore: remove unused

--- packages/client/src/layout/app-layout/AppLayout.tsx
+++ packages/client/src/layout/app-layout/AppLayout.tsx
@@ -2,11 +2,9 @@ import { FC } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import { PageWrapper } from '@/components'
-import { GameOver } from '@/features/gameOver'
 
 export const AppLayout: FC = (): JSX.Element => (
   <PageWrapper columns={1}>
-    <GameOver isOpenned={true}></GameOver>
     <Outlet />
   </PageWrapper>
 )
