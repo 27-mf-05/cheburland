bad12fafc7abc7deba754329b1d6235d51403750
DKS <dksgroup@yandex.ru>
2023-07-07 23:14:53 +0300

    feat: add Auth Service

--- packages/client/src/app/api/endpoints.ts
+++ packages/client/src/app/api/endpoints.ts
@@ -4,3 +4,9 @@ export const USER_PASSWORD_PATH = `${USER_PATH}/password`
 
 export const USER_PROFILE_PATH = `${USER_PATH}/profile`
 export const USER_AVATAR_PATH = `${USER_PROFILE_PATH}/avatar`
+
+export const AUTH_PATH = 'auth'
+export const AUTH_SIGNIN_PATH = `${AUTH_PATH}/signin`
+export const AUTH_SIGNUP_PATH = `${AUTH_PATH}/signup`
+export const AUTH_USER_PATH = `${AUTH_PATH}/user`
+export const AUTH_LOGOUT_PATH = `${AUTH_PATH}/logout`
---
+++ packages/client/src/shared/types/auth.ts
@@ -0,0 +1,13 @@
+export interface SigninData {
+  login: string
+  password: string
+}
+
+export interface SignupData {
+  first_name: string
+  second_name: string
+  login: string
+  email: string
+  password: string
+  phone: string
+}
