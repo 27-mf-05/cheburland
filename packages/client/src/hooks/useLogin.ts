87a85a52a8d566f789ee2f08befbf34580602ae8
DKS <dksgroup@yandex.ru>
2023-07-12 01:21:06 +0300

    feat: add hook useLogin, refactor Login component

---
+++ packages/client/src/hooks/useLogin.ts
@@ -0,0 +1,30 @@
+import { useCallback, useContext } from 'react'
+
+import AuthService from '@/app/api/services/auth.service'
+import { AuthContext } from '@/app/context/AuthContextProvider'
+import { useApiMutation } from '@/hooks/useApiMutation'
+import { Password, SigninData } from '@/shared'
+
+export const useLogin = (): {
+  handleLogin: (data: SigninData) => void
+  isLoading: boolean
+} => {
+  const { login } = useContext(AuthContext)
+
+  const { mutate: signin, isLoading } = useApiMutation(
+    ['signin'],
+    (data: SigninData) => AuthService.signin(data),
+    {
+      onSuccess: login,
+    }
+  )
+
+  const handleLogin = useCallback(
+    (data: SigninData) => {
+      signin(data)
+    },
+    [signin]
+  )
+
+  return { isLoading, handleLogin }
+}
