b477b0d2cdbd9a959fe1c285f0dc6f7820e3b9a2
DKS <dksgroup@yandex.ru>
2023-07-12 01:16:30 +0300

    feat: add tostify to hook useErrors, fix work with strings

--- packages/client/src/layout/app-layout/AppLayout.tsx
+++ packages/client/src/layout/app-layout/AppLayout.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react'
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 import { AppWrapper } from '@/components'
+
+import 'react-toastify/dist/ReactToastify.css'
 
 export const AppLayout: FC = (): JSX.Element => (
   <AppWrapper>
     <Outlet />
+    <ToastContainer />
   </AppWrapper>
 )
