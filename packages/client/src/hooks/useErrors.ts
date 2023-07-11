760cfc31a1c7debb8e97cde5b9283316172a1afa
DKS <dksgroup@yandex.ru>
2023-07-12 01:16:30 +0300

    feat: add tostify to hook useErrors, fix work with strings

--- packages/client/src/hooks/useErrors.ts
+++ packages/client/src/hooks/useErrors.ts
@@ -1,14 +1,15 @@
 import { useCallback } from 'react'
+import { toast } from 'react-toastify'
 
 import { AxiosError } from 'axios'
 
 import { getMessage } from '@/shared'
 
 export const useErrors = (): {
-  handleErrors: (error: AxiosError) => void
+  handleErrors: (error: AxiosError | string) => void
 } => {
-  const handleErrors = useCallback((error: AxiosError) => {
-    console.error(getMessage(error))
+  const handleErrors = useCallback((error: AxiosError | string) => {
+    toast.error(getMessage(error))
   }, [])
 
   return {
--- packages/client/src/shared/helpers/errors.ts
+++ packages/client/src/shared/helpers/errors.ts
@@ -1,8 +1,15 @@
 import type { AxiosError, AxiosResponse } from 'axios'
 
 export const DEFAULT_ERROR_MESSAGE = 'Something went wrong!'
+export const WRONG_LOGIN_PASSWORD_MESSAGE = 'Неверный логин или пароль'
 
-export const getMessage = (e: AxiosError): string =>
-  (e.response as AxiosResponse)?.data?.message ||
-  e.message ||
-  DEFAULT_ERROR_MESSAGE
+export const getMessage = (e: AxiosError | string): string => {
+  if (typeof e === 'string') {
+    return e
+  }
+  return (
+    (e.response as AxiosResponse)?.data?.message ||
+    e.message ||
+    DEFAULT_ERROR_MESSAGE
+  )
+}
