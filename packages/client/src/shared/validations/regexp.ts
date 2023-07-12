15d627b3efda2d9f4a7645bf7d60264bee0ef717
DKS <dksgroup@yandex.ru>
2023-07-12 15:46:47 +0300

    fix: wrong broken email and phone validations

--- packages/client/src/shared/validations/regexp.ts
+++ packages/client/src/shared/validations/regexp.ts
@@ -1,8 +1,7 @@
 export const firstNameRegexp = /^([A-Za-zА-Яа-яЁё][A-Za-zА-Яа-яЁё-]*)$/
 export const secondNameRegexp = /^([A-Za-zА-Яа-яЁё][A-Za-zА-Яа-яЁё-]*)$/
 export const loginRegexp = /^(?!.*[-_]{2})[a-zA-Z0-9_-]{3,20}$/
-export const emailRegexp =
-  /^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\\.[a-zA-Z]{2,}$/
+export const emailRegexp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
 export const passwordRegexp = /^(?=.*[A-Z])(?=.*\d).{8,40}$/
-export const phoneRegexp = /^\\+?\\d{10,15}$/
+export const phoneRegexp = /^\+?\d{10,15}$/
