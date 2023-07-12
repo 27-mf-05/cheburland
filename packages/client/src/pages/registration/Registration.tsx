24b4b718ef4e011d1aa1502b046f1400062ec15a
DKS <dksgroup@yandex.ru>
2023-07-12 16:00:07 +0300

    feat: add useRegistration hook, finish registration logic

--- packages/client/src/pages/registration/Registration.tsx
+++ packages/client/src/pages/registration/Registration.tsx
@@ -1,19 +1,18 @@
 import { Title } from '@mantine/core'
 
 import { FormWrapper } from '@/components'
+import { useRegistration } from '@/hooks'
 import { RegistrationForm } from '@/pages/registration/components'
 
 export const Registration = (): JSX.Element => {
+  const { handleRegistration } = useRegistration()
+
   return (
     <FormWrapper formId="registration">
       <Title align="center" mb={16}>
         Регистрация
       </Title>
-      <RegistrationForm
-        handleSubmit={data => {
-          console.log(data)
-        }}
-      />
+      <RegistrationForm handleSubmit={handleRegistration} />
     </FormWrapper>
   )
 }
