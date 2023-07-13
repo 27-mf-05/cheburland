9927db894edb73db5e82636935f131b1fec573df
DKS <dksgroup@yandex.ru>
2023-07-13 20:09:06 +0300

    feat: user from store on profile page. Fix avatar url

--- packages/client/src/pages/profile/Profile.tsx
+++ packages/client/src/pages/profile/Profile.tsx
@@ -2,50 +2,46 @@ import { Link } from 'react-router-dom'
 
 import { Anchor, Flex, Paper, SimpleGrid, Title } from '@mantine/core'
 
+import { RESOURCES_URL } from '@/app/api/axios'
 import { useRoutes } from '@/hooks'
-import { User } from '@/shared'
+import { useAppSelector } from '@/hooks/useAppSelector'
 
 import { ProfileData, ProfileHeader } from './components'
 
-const user: User = {
-  first_name: 'John',
-  second_name: 'Doe',
-  email: 'pochta@yandex.ru',
-  phone: '7 909 967 30 30',
-  login: 'JDoe',
-  id: 123,
-  display_name: 'John',
-  avatar: '',
-}
-
 export const Profile = (): JSX.Element => {
   const { paths } = useRoutes()
+  const user = useAppSelector(state => state.user.currentUser)!
 
   return (
-    <Flex
-      id="profile"
-      py={16}
-      justify="center"
-      align="center"
-      direction="column">
-      <Paper shadow="xs" py="md" w="100vh" px="xl">
-        <Title mb="xl" ta="center">
-          Профиль пользователя
-        </Title>
-        <ProfileHeader userName={user.first_name} avatar={user.avatar} />
-        <ProfileData user={user} />
-        <SimpleGrid cols={1}>
-          <Anchor component={Link} to={'#'}>
-            Изменить данные
-          </Anchor>
-          <Anchor component={Link} to={paths.ChangePassword}>
-            Изменить пароль
-          </Anchor>
-          <Anchor component={Link} to={'#'} color="red">
-            Выйти
-          </Anchor>
-        </SimpleGrid>
-      </Paper>
-    </Flex>
+    user && (
+      <Flex
+        id="profile"
+        py={16}
+        justify="center"
+        align="center"
+        direction="column">
+        <Paper shadow="xs" py="md" w="100vh" px="xl">
+          <Title mb="xl" ta="center">
+            Профиль пользователя
+          </Title>
+          <ProfileHeader
+            userName={user.first_name}
+            avatar={RESOURCES_URL + user.avatar}
+          />
+          <ProfileData user={user} />
+          <SimpleGrid cols={1}>
+            <Anchor component={Link} to={'#'}>
+              Изменить данные
+            </Anchor>
+            <Anchor component={Link} to={paths.ChangePassword}>
+              Изменить пароль
+            </Anchor>
+            <Anchor component={Link} to={'#'} color="red">
+              Выйти
+            </Anchor>
+          </SimpleGrid>
+        </Paper>
+      </Flex>
+    )
   )
 }
