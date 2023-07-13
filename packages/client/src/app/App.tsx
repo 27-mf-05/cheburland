960076b81930e8343620ca1f590961e2a90de611
DKS <dksgroup@yandex.ru>
2023-07-13 20:05:43 +0300

    feat: add store

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -1,18 +1,24 @@
+import { Provider } from 'react-redux'
+
 import { MantineProvider } from '@mantine/core'
 
 import { AuthContextProvider } from '@/app/context/AuthContextProvider'
+import { setupStore } from '@/app/redux/store'
 import { AppRoutes } from '@/app/routes'
 import { theme } from '@/app/theme'
 
 import QueryClientProvider from './api/QueryClientProvider'
 
+const store = setupStore()
+
 const App = () => (
   <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-    {/* TODO: should be removed */}
     <QueryClientProvider>
-      <AuthContextProvider>
-        <AppRoutes />
-      </AuthContextProvider>
+      <Provider store={store}>
+        <AuthContextProvider>
+          <AppRoutes />
+        </AuthContextProvider>
+      </Provider>
     </QueryClientProvider>
   </MantineProvider>
 )
