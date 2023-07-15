c18dece6fddbd982f39e11272d885ed58b0a84ff
Helena Suslenkova <helena.suslenkova@gmail.com>
2023-07-15 22:10:16 +0300

    chore: refactoring

--- packages/client/src/AppServiceWorker.ts
+++ packages/client/src/AppServiceWorker.ts
@@ -1,48 +1,39 @@
 const DEFAULT_SW_FILE_NAME = 'service-worker.js'
-
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    window.location.hostname === '[::1]' ||
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-)
-
 class AppServiceWorker {
-  private _swFileName: string
+  protected _swFileName: string
+  protected _serviceWorker: ServiceWorkerContainer | undefined
 
   constructor(swFileName: string) {
+    if ('serviceWorker' in navigator) {
+      this._serviceWorker = navigator.serviceWorker
+    }
     this._swFileName = swFileName
   }
 
   register() {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker
-          .register(this._swFileName, { scope: '/' })
-          .then(registration => {
-            console.log(
-              'ServiceWorker registration successful with scope: ',
-              registration.scope
-            )
-          })
-          .catch((error: string) => {
-            console.error('ServiceWorker registration failed: ', error)
-          })
-      })
-    }
+    window.addEventListener('load', () => {
+      this._serviceWorker
+        ?.register(this._swFileName, { scope: '/' })
+        .then(registration => {
+          console.log(
+            'ServiceWorker registration successful with scope: ',
+            registration.scope
+          )
+        })
+        .catch((error: string) => {
+          console.error('ServiceWorker registration failed: ', error)
+        })
+    })
   }
 
   unregister() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.ready
-        .then(registration => {
-          registration.unregister().then(() => 'ServiceWorker unregister')
-        })
-        .catch(error => {
-          console.error(error.message)
-        })
-    }
+    this._serviceWorker?.ready
+      .then(registration => {
+        registration.unregister().then(() => 'ServiceWorker unregister')
+      })
+      .catch(error => {
+        console.error(error.message)
+      })
   }
 }
 
