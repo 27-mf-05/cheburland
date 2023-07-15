45b1d05ae0a2584f5dcacb7cfeda005a9d92d047
aaa15test <aaa15test15aaa@mail.ru>
2023-07-15 09:48:35 +0300

    chore: return to previous state

--- packages/client/src/pages/game/Game.tsx
+++ packages/client/src/pages/game/Game.tsx
@@ -3,7 +3,6 @@ import { ModalsProvider } from '@mantine/modals'
 
 import cheburashka from '@/assets/images/cheburashka.png'
 import maze from '@/assets/images/maze.png'
-import { Game as GameCore } from '@/core/game'
 
 import { gameDescription } from './constants'
 
@@ -12,8 +11,7 @@ export const Game = (): JSX.Element => (
     <Stack id="game" h="100%">
       <Paper shadow="xs" py="md" px="xl" h="100%">
         <Stack p={32}>
-          <GameCore></GameCore>
-          {/* <Text>{gameDescription}</Text>
+          <Text>{gameDescription}</Text>
           <Flex>
             <Image width={240} mx="auto" src={cheburashka} alt="Cheburashka" />
             <Image
@@ -27,7 +25,7 @@ export const Game = (): JSX.Element => (
           </Flex>
           <Button mx="auto" mt="xl" size="lg">
             Начать
-          </Button> */}
+          </Button>
         </Stack>
       </Paper>
     </Stack>
