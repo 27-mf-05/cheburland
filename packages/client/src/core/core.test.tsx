2fc6431facdf0cfb119e20f0a2cda24c20881b2a
Sergey Mironov <sergeymironov@protonmail.com>
2023-07-18 19:49:08 +0300

    fix: fix tests for Game component

--- packages/client/src/core/core.test.tsx
+++ packages/client/src/core/core.test.tsx
@@ -7,7 +7,14 @@ import { Game } from '@/core'
 describe('Core tests', () => {
   it('should throw an error on incorrect value of props rowsAndColumns', () => {
     try {
-      render(<Game rowsAndColumns={17} />)
+      render(
+        <Game
+          onCollision={() => 0}
+          rowsAndColumns={17}
+          cellSize={56}
+          erasers={10}
+        />
+      )
     } catch (error: unknown) {
       expect(error).toBeDefined()
       expect((error as Error).message).toBe(
@@ -16,7 +23,14 @@ describe('Core tests', () => {
     }
 
     try {
-      render(<Game rowsAndColumns={3} />)
+      render(
+        <Game
+          onCollision={() => 0}
+          rowsAndColumns={3}
+          cellSize={56}
+          erasers={10}
+        />
+      )
     } catch (error: unknown) {
       expect(error).toBeDefined()
       expect((error as Error).message).toBe(
@@ -25,7 +39,14 @@ describe('Core tests', () => {
     }
 
     try {
-      render(<Game rowsAndColumns={10} />)
+      render(
+        <Game
+          onCollision={() => 0}
+          rowsAndColumns={10}
+          cellSize={56}
+          erasers={10}
+        />
+      )
     } catch (error: unknown) {
       expect(error).toBeDefined()
       expect((error as Error).message).toBe(
@@ -35,13 +56,27 @@ describe('Core tests', () => {
   })
   it('should throw an error on incorrect value of props cellSize', () => {
     try {
-      render(<Game cellSize={9} />)
+      render(
+        <Game
+          onCollision={() => 0}
+          rowsAndColumns={15}
+          cellSize={9}
+          erasers={10}
+        />
+      )
     } catch (error: unknown) {
       expect(error).toBeDefined()
       expect((error as Error).message).toBe('cellSize should be <125, >10')
     }
     try {
-      render(<Game cellSize={126} />)
+      render(
+        <Game
+          onCollision={() => 0}
+          rowsAndColumns={15}
+          cellSize={126}
+          erasers={10}
+        />
+      )
     } catch (error: unknown) {
       expect(error).toBeDefined()
       expect((error as Error).message).toBe('cellSize should be <125, >10')
