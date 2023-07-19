cc3d25b7a9c9dddb9725dc1029b08340a67a32cb
DKS <dksgroup@yandex.ru>
2023-07-19 17:45:10 +0300

    fix: tests

--- packages/client/src/shared/helpers/errors.ts
+++ packages/client/src/shared/helpers/errors.ts
@@ -1,11 +1,5 @@
-import type { AxiosError, AxiosResponse } from 'axios'
-
 export const DEFAULT_ERROR_MESSAGE = 'Something went wrong!'
 
-export const getMessage = (e: AxiosError): string => {
-  return (
-    (e.response as AxiosResponse)?.data?.message ||
-    e.message ||
-    DEFAULT_ERROR_MESSAGE
-  )
+export const getMessage = (e: Error): string => {
+  return e.message || DEFAULT_ERROR_MESSAGE
 }
