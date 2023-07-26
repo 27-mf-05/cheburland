27a3a16475348108f10c5fb634faad0dde90e26e
Helena Suslenkova <helena.suslenkova@gmail.com>
2023-07-26 20:04:48 +0300

    fix: fix Game page styles

--- packages/client/src/pages/game/components/game-info/GameInfo.tsx
+++ packages/client/src/pages/game/components/game-info/GameInfo.tsx
@@ -13,30 +13,18 @@ type GameInfoProps = {
 export const GameInfo: FC<GameInfoProps> = memo(
   ({ onModalStart }): JSX.Element => {
     return (
-      <Stack align="center" justify="center" h="100vh" p={32}>
+      <Stack p={32}>
+        <Flex p={32} w={'50%'} m={'auto'}>
+          <Text align="center">{GAME_DESCRIPTION}</Text>
+        </Flex>
         <Flex p={32}>
-          <Stack w={'50%'}>
-            <Flex p={32}>
-              <Text align="center">{GAME_DESCRIPTION}</Text>
-            </Flex>
-
-            <Flex p={32}>
-              <Image maw={240} mx="auto" src={cheburashka} alt="Cheburashka" />
-              <Image maw={240} mx="auto" src={maze} alt="Maze" />
-            </Flex>
-            <Flex p={32}>
-              <Button fullWidth size="lg" onClick={onModalStart}>
-                Начать
-              </Button>
-            </Flex>
-          </Stack>
-
-          {/* TODO: add game settings block */}
-          <Stack w="50%" h="100%">
-            <Text p={32} align="center">
-              Game settings block
-            </Text>
-          </Stack>
+          <Image maw={240} mx="auto" src={cheburashka} alt="Cheburashka" />
+          <Image maw={240} mx="auto" src={maze} alt="Maze" />
+        </Flex>
+        <Flex p={32}>
+          <Button w={'50%'} m={'auto'} size="lg" onClick={onModalStart}>
+            Начать
+          </Button>
         </Flex>
       </Stack>
     )
