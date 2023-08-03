a73040c30bfd0a29c5f81c823aa3e717f2fb545c
GeorgyPasyuk <115622303+GeorgyPasyuk@users.noreply.github.com>
2023-08-03 22:12:21 +0300

    chore: fix

--- packages/client/src/components/notification/Notification.tsx
+++ packages/client/src/components/notification/Notification.tsx
@@ -34,7 +34,6 @@ export const NotificationComponent = () => {
 
     toast.onChange(e => {
       e.status === 'removed' ? handleNotificationClose() : {}
-      console.log(e.status)
     })
 
     if (notificationStatus === NotificationStatus.GRANTED) {
