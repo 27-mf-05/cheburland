d837561e75cd400d991c38680df8b7813ab2d675
GeorgyPasyuk <115622303+GeorgyPasyuk@users.noreply.github.com>
2023-08-19 19:23:35 +0300

    fix

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,7 +8,7 @@ import { startApp } from './orm'
 import topicRouter from './orm/routes/topicRoutes/topicRoutes'
 const app = express()
 app.use(cors())
-const port = Number(process.env.SERVER_PORT) || 3001
+const port = Number(process.env.SERVER_PORT) || 3000
 
 startApp()
 
