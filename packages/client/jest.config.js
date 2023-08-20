e41600564e8c77fc6c9c566d6151d9cc68b2696a
Katarina Shalagina <j.milk@yandex.ru>
2023-08-20 17:54:12 +0300

    fix: remove non-pr file

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -10,9 +10,9 @@ export default {
   },
   modulePaths: ['<rootDir>/src', '<rootDir>'],
   moduleNameMapper: {
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/mocks/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/src/mocks/fileMock.js',
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/mocks/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/src/mocks/fileMock.js",
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 }
+
